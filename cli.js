63daec7a992a3dad3d8567fcaee5288ea8d53702
Isaiah Harris <imharris@unc.edu>
2023-02-03 10:25:51 -0500

    reads command line arguments

--- cli.js
+++ cli.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+import minimist from 'minimist';
+import fetch from 'node-fetch';
+import timezone from 'moment-timezone';
+
+const args = minimist(process.argv.slice(2));
+console.log(args);
